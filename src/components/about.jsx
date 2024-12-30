19103c98ebe885a11eafde38248698519f777525
kumarChandan <ck368966@gmail.com>
2024-12-31 00:27:00 +0530

    do change

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,8 @@
 export const About = ()=>{
     return(
         <div>
-            <h1> Hi, Chandan This side</h1>
+            <h1> Hi, Chandan this side</h1>
+            <p>This project is under development......</p>
         </div>
     )
 }
